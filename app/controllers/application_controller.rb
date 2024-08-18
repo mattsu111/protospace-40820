b79016fb6b7e77a4eb11d35c6927397703f15837
mattsu111 <songjingjianwu5@gmail.com>
2024-08-18 15:58:07 +0900

    完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :position, :occupation])
+  end
+
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      @current_user = nil
+    end
+
+
+  end
 end
